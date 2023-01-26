e451c2c1f2d0cac15e9b41651cac3fd1621ef67f
Ларина Марина <mlarina@domain.tkmistral.ru>
2023-01-26 12:02:44 +0300

    files

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,18 @@
+import Validator from '../app';
+
+test('works correctly', () => {
+  expect(Boolean(Validator.validateUsername)).toBeTruthy();
+  expect(Validator.validateUsername('aBc123-456xYz')).toBeTruthy();
+  expect(Validator.validateUsername('gIj78_90uVw')).toBeTruthy();
+  expect(Validator.validateUsername('-name')).toBeFalsy();
+  expect(Validator.validateUsername('name-')).toBeFalsy();
+  expect(Validator.validateUsername('_name')).toBeFalsy();
+  expect(Validator.validateUsername('name_')).toBeFalsy();
+  expect(Validator.validateUsername('1name')).toBeFalsy();
+  expect(Validator.validateUsername('name1')).toBeFalsy();
+  expect(Validator.validateUsername('na1234me')).toBeFalsy();
+  expect(Validator.validateUsername('na.me')).toBeFalsy();
+  expect(Validator.validateUsername('na!me')).toBeFalsy();
+  expect(Validator.validateUsername('na[me')).toBeFalsy();
+  expect(Validator.validateUsername('na|me')).toBeFalsy();
+});
